b5bdb8ea9bd295d930112a38a5a675b7405f756d
Fabio Mora <mail@fabio.mora.name>
2013-02-20 23:44:34 +0100

    Green testGet() with a mock.

--- tests/AbstractFunctionalTestTest.php
+++ tests/AbstractFunctionalTestTest.php
@@ -3,21 +3,13 @@
  * @author Jacopo 'Jakuza' Romei <jromei@gmail.com>
  */
 
-class ConcreteWrapper extends \Avoja\Test\AbstractFunctionalTest
-{
-    public function get($url)
-    {
-        return parent::get($url);
-    }
-}
-
 /**
  * Unit tests for \Avoja\Test\AbstractFunctionalTest class
  */
 class AbstractFunctionalTestTest extends \PHPUnit_Framework_TestCase {
 
     public function testGet() {
-        $test = new ConcreteWrapper();
+        $test = new \ConcreteWrapper();
 
         $this->assertInstanceOf('\Avoja\Test\Crawler', $test->get('http://www.google.com/'));
     }
--- tests/ConcreteWrapper.php
+++ tests/ConcreteWrapper.php
@@ -1,3 +1,10 @@
 <?php
 
-
+class ConcreteWrapper extends \Avoja\Test\AbstractFunctionalTest
+{
+    public function get($url)
+    {
+        //return parent::get($url);
+        return $this->getMock('\Avoja\Test\Crawler', array('get'));
+    }
+}
